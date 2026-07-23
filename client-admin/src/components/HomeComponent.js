931d8185f15a43600d58cff5d6f8bc2add8b7cce
PhamHoangAnn <128809543+PhamHoangAnn@users.noreply.github.com>
2026-07-23 08:40:35 +0700

    Update image source in HomeComponent

--- client-admin/src/components/HomeComponent.js
+++ client-admin/src/components/HomeComponent.js
@@ -5,7 +5,7 @@ class Home extends Component {
     return (
     <div className="align-center">
     <h2 className="text-center">ADMIN HOME</h2>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKUUJ6WxKhcOpMJv3Cqc6hZDxr3iej3JjyxRqp3lF7zg&s=10" width="800px" height="600px" alt="" />
+    <img src="https://www.facebook.com/logochibicute/posts/t%E1%BB%95ng-h%E1%BB%A3p-logo-tr%C3%A0-s%E1%BB%AFa-si%C3%AAu-xinh-%C4%83n-v%E1%BA%B7t-tr%C3%A0-s%E1%BB%AFa-lu%C3%B4n-l%C3%A0-m%E1%BB%99t-trong-nh%E1%BB%AFng-ng%C3%A0nh-h%C3%A0n/578206791029232/" width="800px" height="600px" alt="" />
 </div>
 );
 }
