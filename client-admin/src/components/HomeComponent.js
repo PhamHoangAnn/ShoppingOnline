a1946667ec8725996d8702d52fa92bec44eed621
PhamHoangAnn <128809543+PhamHoangAnn@users.noreply.github.com>
2026-07-23 08:43:52 +0700

    Update HomeComponent.js

--- client-admin/src/components/HomeComponent.js
+++ client-admin/src/components/HomeComponent.js
@@ -5,7 +5,7 @@ class Home extends Component {
     return (
     <div className="align-center">
     <h2 className="text-center">ADMIN HOME</h2>
-    <img src="https://www.facebook.com/logochibicute/posts/t%E1%BB%95ng-h%E1%BB%A3p-logo-tr%C3%A0-s%E1%BB%AFa-si%C3%AAu-xinh-%C4%83n-v%E1%BA%B7t-tr%C3%A0-s%E1%BB%AFa-lu%C3%B4n-l%C3%A0-m%E1%BB%99t-trong-nh%E1%BB%AFng-ng%C3%A0nh-h%C3%A0n/578206791029232/" width="800px" height="600px" alt="" />
+    <img src="https://img.tripi.vn/cdn-cgi/image/width=700,height=700/https://img5.thuthuatphanmem.vn/uploads/2022/01/13/logo-cua-hang-tra-sua_082029941.jpg" width="800px" height="600px" alt="" />
 </div>
 );
 }
